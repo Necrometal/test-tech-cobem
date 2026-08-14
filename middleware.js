4eabf1b3332b458126d034677004b7b4485fedea
Necrometal <gisenael.dev44@gmail.com>
2026-08-14 16:07:26 +0300

    feat: enforce token validation with jose

--- middleware.js
+++ middleware.js
@@ -1,7 +1,8 @@
 import { NextResponse } from 'next/server';
+import { checkToken } from './app/lib/auth';
  
 // This function can be marked `async` if using `await` inside
-export function middleware(request) {
+export async function middleware(request) {
   const authHeader = request.headers.get("authorization");
   const token = authHeader?.startsWith("Bearer ") ? authHeader.split(" ")[1] : null;
 
@@ -9,6 +10,16 @@ export function middleware(request) {
     { error: "Not Authenticated" },
     { status: 401 }
   )
+
+  const payload = await checkToken(token)
+
+  if(!payload){
+    return NextResponse.json(
+      { error: "Invalid or expired token" },
+      { status: 401 }
+    )
+  }
+
   return NextResponse.next()
 }
  
